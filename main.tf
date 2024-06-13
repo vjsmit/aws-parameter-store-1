7f8e309ab38445e5ce0efa4378bd68daf6b5d583
smvijay <vj.smit97@gmail.com>
2024-06-13 04:30:46 -0400

    tf output

--- main.tf
+++ main.tf
@@ -12,5 +12,10 @@ variable "params" {
     {name= "roboshop.dev.frontend.cart_url",value = "http://cart-dev.smitdevops.store:8080/", type = "String"},
     {name= "roboshop.dev.frontend.shipping_url",value = "http://shipping-dev.smitdevops.store:8080/", type = "String"},
     {name= "roboshop.dev.frontend.payment_url",value = "http://payment-dev.smitdevops.store:8080/", type = "String"},
+    {name= "roboshop.dev.cart.redis_host",value = "redis-dev.smitdevops.store:8080/", type = "String"},
+    {name= "roboshop.dev.cart.catalogue_host",value = "catalogue-dev.smitdevops.store:8080/", type = "String"},
+    {name= "roboshop.dev.cart.catalogue_port",value = "8080", type = "String"},
+    {name= "roboshop.dev.catalogue.mongo",value = "true", type = "String"},
+    {name= "roboshop.dev.catalogue.mongo_url",value = "mongo-dev.smitdevops.store", type = "String"},
   ]
 }
